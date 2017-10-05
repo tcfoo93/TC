7e26a923ae1955f3b3e4e5ef55d785e93e34f1c2
foo thin chun <tcfoo93@gmail.com>
2017-10-05 16:16:49 +0800

    Initial commit

---
+++ app/src/main/java/org/dev/tcfoo/tc/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package org.dev.tcfoo.tc.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.dev.tcfoo.tc.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/org/dev/tcfoo/tc/Activities/Splash.java
@@ -0,0 +1,15 @@
+package org.dev.tcfoo.tc.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.dev.tcfoo.tc.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
